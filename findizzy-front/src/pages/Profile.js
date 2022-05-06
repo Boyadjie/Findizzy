2f91692ef3292b7a23d111060944226e884ef684
emeric boyadjian <emericboy73@gmail.com>
2022-05-06 10:32:19 +0200

    Add user pets informations in Profile front (name and type + skeleton)

--- findizzy-front/src/pages/Profile.js
+++ findizzy-front/src/pages/Profile.js
@@ -4,10 +4,13 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Box from '@mui/material/Box';
 import Skeleton from '@mui/material/Skeleton';
 import Stack from '@mui/material/Stack';
+import Avatar from '@mui/material/Avatar';
 
 const Profile = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  const [userLoading, setUserLoading] = useState(true);
+  const [petLoading, setPetLoading] = useState(true);
   const [user, setUser] = useState(null);
+  const [pets, setPets] = useState(null);
 
     useEffect(() => {
       fetch("http://localhost:1337/api/users/1", 
@@ -21,26 +24,83 @@ const Profile = () => {
       .then(response => {
         setTimeout(() => {
           setUser(response);
-          setIsLoading(false);
+          setUserLoading(false);
         }, 2000)
       })
       .catch(err => {
         console.log(err);
       });
     }, [])
+
+    useEffect(() => {
+      fetch("http://localhost:1337/api/pets?user=1", 
+      {
+        method: "GET",
+        headers: {
+          'Accept': 'Application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(response => {
+        setTimeout(() => {
+          setPets(response);
+          setPetLoading(false);
+        }, 2000)
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }, [])
+
   return (
     <div>
       <a className='' href="/"><ArrowBackIcon sx={{ fontSize: 40 }} /></a>
       <h1>Mon Profil</h1>
       <div className='profileInformations'>
-        { isLoading == true ? (
+        { userLoading === true ? (
           <Box>
             <Stack spacing={1}>
               <Skeleton variant="circular" width={40} height={40} />
-              <Skeleton variant="rectangular" width={210} height={118} />
+              <Skeleton variant="rectangular" width={50} height={10} />
+              <Skeleton variant="rectangular" width={100} height={10} />
             </Stack>
           </Box>
-        ) : user.username }
+        ) : (
+          <Box>
+            <Stack spacing={1}>
+              <Avatar alt="User Avatar" src="" />
+              <h3> { user.username } </h3>
+              <h2> { user.email } </h2>
+            </Stack>
+          </Box>
+        ) }
+      </div>
+
+      <div className='profileInformations'>
+        { petLoading === true ? (
+          <Box>
+            <Stack spacing={1}>
+              <Skeleton variant="rectangular" width={200} height={40} />
+              <Skeleton variant="rectangular" width={200} height={40} />
+            </Stack>
+          </Box>
+        ) : (
+          <Box>
+            <Stack spacing={1}>
+              { pets.data.map((pet) => (
+                <Box>
+                  <Stack direction="row" spacing={1}>
+                    <Avatar alt="Pet Avatar" src="" />
+                    <Stack>
+                      <p>{ pet.attributes.name }</p>
+                      <p>{ pet.attributes.type }</p>
+                    </Stack>
+                  </Stack>
+                </Box>
+              ))}
+            </Stack>
+          </Box>
+        ) }
       </div>
     </div>
   );
