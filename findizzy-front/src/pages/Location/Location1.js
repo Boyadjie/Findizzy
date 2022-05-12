476f032c4e2676e9a2146b51e9b2d2803ce11559
Gresse Baptiste <Baptiste.gresse@epitech.digital>
2022-05-12 19:57:07 +0200

    batti 2.0

---
+++ findizzy-front/src/pages/Location/Location1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import React from 'react';
+
+const Location1 = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Location1;
