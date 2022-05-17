f55824cbf0017127ef4bcb429715e6fe5e1ebb80
Gresse Baptiste <Baptiste.gresse@epitech.digital>
2022-05-17 14:10:08 +0200

    help

--- findizzy-front/src/pages/Location/ScanInfo.js
+++ findizzy-front/src/pages/Location/ScanInfo.js
@@ -1,6 +1,26 @@
 import React from 'react';
 
 const ScanInfo = () => {
+
+    let searchParams = parseInt(window.location.search.slice(7));
+
+    const [scanInfo, setscanInfo] = useState(null);
+    const [ScanLoading, setScanLoading] = useState(true);
+
+    let [searchParams] = useSearchParams();
+
+
+    useEffect(() => {
+        fetchscanInfo(searchParams);
+      }, [searchParams]);
+    
+      const fetchscanInfo =  async (id) => {
+        const myUser = await scanApi.getScan(id);
+        setTimeout(() => {
+          setscanInfo(myUser);
+          setScanLoading(false);
+        }, 500);
+
     return (
         <div>
             
