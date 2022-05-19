cddfe89abfd3d46fa64b72c329b191e888040ac8
Gresse Baptiste <Baptiste.gresse@epitech.digital>
2022-05-19 14:55:22 +0200

    add by baptiste gresse geolocation

--- findizzy-front/src/pages/Location/ScanInfo.js
+++ findizzy-front/src/pages/Location/ScanInfo.js
@@ -2,15 +2,23 @@ import React, { useState, useEffect } from 'react';
 import FormLocate from '../../components/forms/FormLocate';
 import scanApi from '../../services/scanApi';
 
+
 const ScanInfo = () => {
-    let searchParams = parseInt(window.location.search.slice(7));
 
-    const [scanInfo, setscanInfo] = useState(null);
-    const [scanLoading, setScanLoading] = useState(true);
+  let searchParams = parseInt(window.location.search.slice(7));
 
+  const [scanInfo, setscanInfo] = useState(null);
+  const [scanLoading, setScanLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [locationLoading, setLocationLoading] = useState(true);
 
     useEffect(() => {
         fetchscanInfo(searchParams);
+        if(navigator.geolocation) {
+          fetchLocation();
+        }else {
+          alert('It seems like Geolocation, which is required for this page, is not enabled in your browser. Please use a browser which supports it.');
+        }
       }, [searchParams]);
     
       const fetchscanInfo =  async (id) => {
@@ -21,10 +29,23 @@ const ScanInfo = () => {
         }, 500);
       }
 
+      const fetchLocation =  async () => {
+        await navigator.geolocation.getCurrentPosition((position) => {
+          const lat = position.coords.latitude;
+          const long = position.coords.longitude;
+          setTimeout(() => {
+            setLocation('latitude:'+lat+' longitude :'+long);
+            setLocationLoading(false);
+          }, 500);
+        });
+      }
+
     return (
         <div>
           {scanLoading ? console.log("") : (
-            <FormLocate mailTo={scanInfo} location="coordonnées" />
+            locationLoading ? console.log('') : (
+              <FormLocate mailTo={scanInfo} location={location} />
+            )
           )}
         </div>
     );
